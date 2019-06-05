44e0fb0ce78583ae193c03c9914006931ddcfda8
Mikhail Belov <mbelov@os-company.ru>
2019-06-06 00:43:38 +0500

    чистка лишнего вывода

--- stationsInfo/urls.py
+++ stationsInfo/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
+    url('stats', views.stats),
     url('', views.info),
 ]
--- stationsInfo/views.py
+++ stationsInfo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import os
 import json
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 def info(request):
     date = request.GET['date']
@@ -19,5 +19,17 @@ def info(request):
                 toJsonArr[js['code']] = {'name':js['name'], 'coords':js['coords'], 'eff':js['eff'][date]}
         except Exception as e:
             print("error in ", file, e)	
-    print(json.dumps(toJsonArr))			
+    #print(json.dumps(toJsonArr))			
     return HttpResponse(json.dumps(toJsonArr))
+	
+def stats(request):
+    code = request.GET['code']
+    dir = os.path.dirname(os.path.abspath(__file__))
+    filename = dir+'/Downloads/'+code
+    try:
+        f = open(filename, 'r')
+        js = json.load(f)
+        f.close()
+        return HttpResponse(json.dumps(js['eff']))
+    except:
+       return HttpResponseNotFound('404')
